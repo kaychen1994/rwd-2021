060f1a50174409400f7cfd9cbd406ecd0526758d
kaychen1994 <kay830123@gmail.com>
2021-03-01 20:27:53 +0800

    deploy

---
+++ app/assets/js/all.js
@@ -0,0 +1,14 @@
+let swiperBanner = new Swiper('.swiperBanner', {
+  autoplay: {
+    delay: 5000,
+    stopOnLastSlide: false
+  },
+});
+let swiperTravel = new Swiper('.swiperTravel', {
+  autoplay: {
+    delay: 5000,
+    stopOnLastSlide: false
+  },
+  slidesPerView: 3,
+  spaceBetween: 30,
+});
